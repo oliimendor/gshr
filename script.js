c992c26ba215e8503822d445d1e59b79d4e60e6a
oliimendor <endor.sensey@gmail.com>
2024-12-12 13:14:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var aboba = 5;
+let lol =  'miya yo';
+const lolez = 155;
+
+
+
+
+
+let  = prompt('dismingizni kiriting');
+let  = prompt('familiyandizni kiriting')
+let = prompt('yosingizni kiriting')
+let = prompt('yoqtirgan faningizni yozing')
+let = prompt('yashayotgan tumaningizni kiriting')
+(`/malimot kiritganingiz uchun rahmat`)
